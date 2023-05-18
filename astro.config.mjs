bd50193d49d1a3e5121b5929c3044369c5019461
Walter Radduso <w.radduso@gmail.com>
2023-05-17 23:58:05 -0300

    add deploy yml and update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: 'https://walterradduso.github.io',
+  base: '/astro-todo-list',
 });
